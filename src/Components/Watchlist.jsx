eaec8be21e9e8dc850a3533b795c1cb3bfbb3ae7
Ankitha <ankithahv31@gmail.com>
2025-07-04 21:22:49 +0530

    watchlist updated

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -31,29 +31,26 @@ function Watchlist({ watchlist, setwatchlist, handleremovefromwatchlist }) {
   useEffect(() => {
     let temp = watchlist.map((movieObj) => genreids[movieObj.genre_ids?.[0]]);
     temp = new Set(temp);
-    temp.delete(undefined); // remove undefined values if any
+    temp.delete(undefined);
     setgenreList(["All Genres", ...temp]);
-    console.log(temp);
   }, [watchlist]);
 
   return (
     <>
-      <div className="flex justify-center flex-wrap m-4 ">
-        {genreList.map((genre, index) => {
-          return (
-            <div
-              key={index}
-              onClick={() => handleFilter(genre)}
-              className={
-                currGenre === genre
-                  ? "flex justify-center items-center h-[3rem] w-[9rem] rounded-xl text-white font-bold bg-blue-400 mx-4"
-                  : "flex justify-center items-center h-[3rem] w-[9rem] rounded-xl text-white font-bold bg-gray-400 mx-4"
-              }
-            >
-              {genre}
-            </div>
-          );
-        })}
+      <div className="flex justify-center flex-wrap m-4">
+        {genreList.map((genre, index) => (
+          <div
+            key={index}
+            onClick={() => handleFilter(genre)}
+            className={
+              currGenre === genre
+                ? "flex justify-center items-center h-[3rem] w-[9rem] rounded-xl text-white font-bold bg-blue-400 mx-2 my-2"
+                : "flex justify-center items-center h-[3rem] w-[9rem] rounded-xl text-white font-bold bg-gray-400 mx-2 my-2"
+            }
+          >
+            {genre}
+          </div>
+        ))}
       </div>
 
       <div className="flex justify-center my-4">
@@ -66,61 +63,58 @@ function Watchlist({ watchlist, setwatchlist, handleremovefromwatchlist }) {
         />
       </div>
 
-      <div className="overflow-hidden rounded=lg border border-gray-200 m-8">
-        <table className="w-full text-center text-gray-500">
+      {/* Scrollable wrapper for table */}
+      <div className="overflow-x-auto mx-4">
+        <table className="w-full min-w-[600px] text-center text-gray-500">
           <thead className="border-b-2">
             <tr>
               <th>Name</th>
-              <th className="flex justify-center">
-                <div onClick={sortIncreasing} className="p-2 cursor-pointer">
+              <th className="flex justify-center items-center gap-2">
+                <div onClick={sortIncreasing} className="cursor-pointer">
                   <i className="fa-solid fa-arrow-up"></i>
                 </div>
-                <div className="p-2">Ratings</div>
-                <div onClick={sortDecreasing} className="p-2 cursor-pointer">
+                Ratings
+                <div onClick={sortDecreasing} className="cursor-pointer">
                   <i className="fa-solid fa-arrow-down"></i>
                 </div>
               </th>
               <th>Popularity</th>
               <th>Genre</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {watchlist
               .filter((movieObj) => {
-                if (currGenre === "All Genres") {
-                  return true;
-                } else {
-                  return genreids[movieObj.genre_ids?.[0]] === currGenre;
-                }
+                return currGenre === "All Genres" ||
+                  genreids[movieObj.genre_ids?.[0]] === currGenre;
               })
-              .filter((movieObj) => {
-                return movieObj.title
+              .filter((movieObj) =>
+                movieObj.title
                   .toLowerCase()
-                  .includes(search.toLowerCase());
-              })
-              .map((movieObj, index) => {
-                return (
-                  <tr className="border-b-2" key={movieObj.id || index}>
-                    <td className="flex items-center px-6 py-4">
-                      <img
-                        className="h-[6rem] w-[10rem]"
-                        src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
-                        alt="movie poster"
-                      />
-                      <div className="mx-10">{movieObj.title}</div>
-                    </td>
-                    <td>{movieObj.vote_average}</td>
-                    <td>{movieObj.popularity}</td>
-                    <td>{genreids[movieObj.genre_ids?.[0]]}</td>
-                    <td
-                      onClick={() => handleremovefromwatchlist(movieObj)}
-                      className="text-red-800 cursor-pointer"
-                    >
-                      Delete
-                    </td>
-                  </tr>
-                );
-              })}
+                  .includes(search.toLowerCase())
+              )
+              .map((movieObj, index) => (
+                <tr className="border-b-2" key={movieObj.id || index}>
+                  <td className="flex items-center px-6 py-4">
+                    <img
+                      className="h-[6rem] w-[10rem]"
+                      src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
+                      alt="movie poster"
+                    />
+                    <div className="mx-4 text-left">{movieObj.title}</div>
+                  </td>
+                  <td>{movieObj.vote_average}</td>
+                  <td>{movieObj.popularity}</td>
+                  <td>{genreids[movieObj.genre_ids?.[0]]}</td>
+                  <td
+                    onClick={() => handleremovefromwatchlist(movieObj)}
+                    className="text-red-800 cursor-pointer"
+                  >
+                    Delete
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
